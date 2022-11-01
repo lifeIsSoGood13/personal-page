b8244b761384b2a36095600e8a61654d6c4a5fb1
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-01 15:12:15 +0300

    Init project & prepare for deploy

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import styles from './App.module.css';
+import image from './images/konstantin.jpeg';
+import tg from './images/telegram.svg';
+import vk from './images/vk.svg';
+
+export const App = () => {
+	return (
+		<section className={styles.app}>
+			<div className={styles.container}>
+				<div className={styles.imageWrapper}>
+					<img className={styles.image} src={image} alt='Константин Антипин' />
+				</div>
+				<div className={styles.descriptionWrapper}>
+					<h1 className={styles.title}>Константин Антипин</h1>
+					<p className={styles.description}>Издатель. Делаю детство добрым</p>
+					<ul className={styles.socialWrapper}>
+						<li className={styles.socialItem}>
+							<a href='https://vk.com/konstantinantipin' target='_blank' rel='noreferrer'>
+								<img className={styles.icon} src={vk} alt='ВКтонтакте' />
+							</a>
+						</li>
+						<li className={styles.socialItem}>
+							<a href='https://t.me/antipin_konstantin' target='_blank' rel='noreferrer'>
+								<img className={styles.icon} src={tg} alt='Телеграмм' />
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	);
+};
